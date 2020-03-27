5e2f0d5e212c9c37389d5b8093451ffe27387de9
sefremidis <sefremidis@athtech.gr>
2020-03-27 22:08:25 +0200

    First commit

---
+++ src/main/java/gr/athtech/App.java
@@ -0,0 +1,12 @@
+package gr.athtech;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Output: \"" + sample() + "\".");
+	}
+	
+	public static String sample() {
+		return "sample";
+	}
+}
---
+++ src/test/java/gr/athtech/AppTest.java
@@ -0,0 +1,14 @@
+package gr.athtech;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertEquals(App.sample(), "sample");
+	}
+
+}
